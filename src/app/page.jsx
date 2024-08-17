19334c141b5811442a2f68aa5742b3a1a51f5799
bhanu-sh <bhanu1234sharma@gmail.com>
2024-08-17 21:11:20 +0530

    Projects Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   return (
     <div>
--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,14 +1,38 @@
 export const projects = [
-    {
-        title: "Weather App",
-        description: "This website shows the current weather conditions of given city, with dynamic background, made using React.js,and openweathermap API.",
-        image: "/images/project1.jpg",
-        link: "#",
-    },
-    {
-        title: "Project 2",
-        description: "This is a project description.",
-        image: "/images/project2.jpg",
-        link: "#",
-    },
+  {
+    title: "CollegeEase",
+    description:
+      "Developed a College Management System, which is being used in Real World. It is made on Next.Js and TypeScript with Next.js API route and MongoDB for Database. Used NextAuth and JWT for authentication and session management. Has features like student management, Staff Management, College Expense Management, Fee Management, import from Excel, export to Excel, Course Management, etc. It’s completely scalable. Used Tailwind and Shadcn for Styling and Responsive Design",
+    image: "/images/project2.jpg",
+    frontend: ["Next.js", "TypeScript", "Tailwind CSS"],
+    backend: ["Next.js API", "MongoDB"],
+    link: "#",
+  },
+  {
+    title: "VoxTales",
+    description:
+      "Developed a Podcast Platform, made on Next.Js and TypeScript with Next.js API route and MongoDB for Database. Used EdgeStore Image and File Bucket for storing Images and Podcasts. Styled with tailwind, it has good UI and Responsive on any Device.",
+    image: "/images/project1.jpg",
+    frontend: ["Next.js", "TypeScript", "Tailwind CSS"],
+    backend: ["Next.js API", "MongoDB"],
+    link: "#",
+  },
+  {
+    title: "ReFurnished",
+    description:
+      "Developed a Web App using MERN stack and AWS S3 storage bucket that focuses on buying and selling of old furniture. Anyone can upload their furniture details and once the price is given by admin, it's automatically listed",
+    image: "/images/project1.jpg",
+    frontend: ["React.js", "Bootstrap"],
+    backend: ["Node.js", "Express.js", "MongoDB", "AWS S3"],
+    link: "#",
+  },
+  {
+    title: "WeatherWave",
+    description:
+      "Developed a Web App for Weather Report using React.js which has location-based weather report and dynamic background for different weathers.",
+    image: "/images/project1.jpg",
+    frontend: ["React.js", "Tailwind CSS"],
+    backend: ["OpenWeatherMap API"],
+    link: "#",
+  },
 ];

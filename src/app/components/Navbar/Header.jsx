20b090899d0f37c9196ecd59af6bf0a4d5e6c5c0
bhanu-sh <bhanu1234sharma@gmail.com>
2024-08-17 20:46:52 +0530

    enabled dark mode

---
+++ src/app/components/Navbar/Header.jsx
@@ -0,0 +1,58 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="bg-background dark:text-neutral-200 flex flex-row justify-around">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Contact
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/lib/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        title: "Weather App",
+        description: "This website shows the current weather conditions of given city, with dynamic background, made using React.js,and openweathermap API.",
+        image: "/images/project1.jpg",
+        link: "#",
+    },
+    {
+        title: "Project 2",
+        description: "This is a project description.",
+        image: "/images/project2.jpg",
+        link: "#",
+    },
+];
